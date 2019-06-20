d4aaf2a0ea67d38ed6f5da8b0ac32cca3d2d0dbf
04041143 <04041143@163.com>
2019-06-21 06:50:39 +0800

    备份代码

--- src/main/java/org/multilinguals/enterprise/cmrs/interfaces/query/UserQueryController.java
+++ src/main/java/org/multilinguals/enterprise/cmrs/interfaces/query/UserQueryController.java
@@ -4,11 +4,13 @@ import org.multilinguals.enterprise.cmrs.infrastructure.dto.QueryResponse;
 import org.multilinguals.enterprise.cmrs.query.user.UserDetailsView;
 import org.multilinguals.enterprise.cmrs.query.user.UserDetailsViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,11 +28,44 @@ public class UserQueryController {
      * @param reqSenderId 请求发起人的用户ID
      * @return 详细信息
      */
-    @GetMapping("/user/self-details")
+    @GetMapping("/user/get-self-details")
     @PreAuthorize("isAuthenticated()")
-    public QueryResponse<UserDetailsView> signUp(@RequestAttribute String reqSenderId) {
+    public QueryResponse<UserDetailsView> querySelfDetails(@RequestAttribute String reqSenderId) {
         Optional<UserDetailsView> userDetailsView = this.userDetailsViewRepository.findById(reqSenderId);
 
         return userDetailsView.map(QueryResponse::new).orElseGet(() -> new QueryResponse<>(null));
+    }
+
+    /**
+     * 管理员查询用户列表
+     *
+     * @param page 当前页码
+     * @param size 页面大小
+     * @return 用户列表
+     */
+    @GetMapping("/admin/get-user-list")
+    @PreAuthorize("hasAnyRole('ROLE_USER_ADMIN','ROLE_SUPER_ADMIN')")
+    public QueryResponse<List<UserDetailsView>> queryUserList(@RequestParam(defaultValue = "0", required = false) String page, @RequestParam(defaultValue = "20", required = false) String size) {
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Page<UserDetailsView> userDetailsViewPage = this.userDetailsViewRepository.findAll(PageRequest.of(Integer.valueOf(page), Integer.valueOf(size), sort));
+        if (userDetailsViewPage.hasContent()) {
+            List<UserDetailsView> userDetailsViewList = userDetailsViewPage.getContent();
+            return new QueryResponse<>(userDetailsViewList);
+        } else {
+            return new QueryResponse<>(null);
+        }
+    }
+
+    /**
+     * 管理员查看其他用户详情
+     * @param id 用户id
+     * @return 用户详情
+     */
+    @GetMapping("/admin/get-user-details/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_USER_ADMIN','ROLE_SUPER_ADMIN')")
+    public QueryResponse<UserDetailsView> queryUserDetails(@PathVariable String id) {
+        Optional<UserDetailsView> userDetailsView = this.userDetailsViewRepository.findById(id);
+
+        return null;
     }
 }
