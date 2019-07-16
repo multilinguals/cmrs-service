e5be3793aa41a8bd79321407a98f1ce1831804a1
t04041143 <t008008>
2019-07-16 16:19:52 +0800

    增加视图仓库

--- src/main/java/org/multilinguals/enterprise/cmrs/query/menuitem/SetMenuItemView.java
+++ src/main/java/org/multilinguals/enterprise/cmrs/query/menuitem/SetMenuItemView.java
@@ -2,12 +2,15 @@ package org.multilinguals.enterprise.cmrs.query.menuitem;
 
 import org.multilinguals.enterprise.cmrs.command.aggregate.restaurant.MenuItemId;
 import org.multilinguals.enterprise.cmrs.command.aggregate.restaurant.SingleMenuItem;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Map;
 
 public class SetMenuItemView {
+    @Id
     private String id;
 
     private String name;
@@ -22,6 +25,7 @@ public class SetMenuItemView {
 
     private BigDecimal totalPrice = BigDecimal.ZERO;
 
+    @Transient
     private Map<MenuItemId, SingleMenuItem> singleMenuItems;
 
     private Date createdAt;
---
+++ src/main/java/org/multilinguals/enterprise/cmrs/query/menuitem/SetMenuItemViewRepository.java
@@ -0,0 +1,6 @@
+package org.multilinguals.enterprise.cmrs.query.menuitem;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface SetMenuItemViewRepository extends MongoRepository<SingleMenuItemView, String> {
+}
