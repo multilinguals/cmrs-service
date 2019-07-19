e22fa7c99c72f719b7e51d70124a3cde1e0c5748
04041143 <04041143@163.com>
2019-07-19 23:31:01 +0800

    增加脚本

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar target/cmrs-service-0.1.0-SNAPSHOT.jar --server.port=9580 &
+nohup java -jar target/cmrs-service-0.1.0-SNAPSHOT.jar>/dev/null 2>&1 --server.port=9580 &
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PID=$(ps -ef | grep cmrs-service-0.1.0-SNAPSHOT.jar | grep -v grep | awk '{ print $2 }')
-if [ -z "$PID" ]
+if [[ -z "$PID" ]]
 then
     echo Application is already stopped
 else
