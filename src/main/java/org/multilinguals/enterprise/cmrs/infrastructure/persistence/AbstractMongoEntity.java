02a7bde910eedd0143363010c6febfce911d17b6
t04041143 <t008008>
2019-07-29 17:56:38 +0800

    移除版本控制

--- src/main/java/org/multilinguals/enterprise/cmrs/infrastructure/persistence/AbstractMongoEntity.java
+++ src/main/java/org/multilinguals/enterprise/cmrs/infrastructure/persistence/AbstractMongoEntity.java
@@ -1,16 +1,4 @@
 package org.multilinguals.enterprise.cmrs.infrastructure.persistence;
 
-import org.springframework.data.annotation.Version;
-
 public abstract class AbstractMongoEntity {
-    @Version
-    private Long version;
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
-    }
 }
