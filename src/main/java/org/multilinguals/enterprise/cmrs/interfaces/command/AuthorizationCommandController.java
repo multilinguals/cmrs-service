34f35a226d5159ccd4f0e06ffb995aab0ab442aa
t04041143 <t008008>
2019-08-05 15:16:47 +0800

    调整本地化文件

--- src/main/java/org/multilinguals/enterprise/cmrs/interfaces/command/AuthorizationCommandController.java
+++ src/main/java/org/multilinguals/enterprise/cmrs/interfaces/command/AuthorizationCommandController.java
@@ -7,12 +7,15 @@ import org.multilinguals.enterprise.cmrs.command.aggregate.user.UserId;
 import org.multilinguals.enterprise.cmrs.command.aggregate.usersession.UserSessionId;
 import org.multilinguals.enterprise.cmrs.command.aggregate.usersession.command.DeleteUserSessionCommand;
 import org.multilinguals.enterprise.cmrs.command.handler.signin.SignInWithPasswordCommand;
+import org.multilinguals.enterprise.cmrs.constant.aggregate.account.AccountType;
 import org.multilinguals.enterprise.cmrs.constant.result.ErrorCode;
 import org.multilinguals.enterprise.cmrs.dto.authorization.UserSignInDTO;
 import org.multilinguals.enterprise.cmrs.infrastructure.data.Tuple2;
 import org.multilinguals.enterprise.cmrs.infrastructure.exception.aggregate.UserPasswordInvalidException;
 import org.multilinguals.enterprise.cmrs.infrastructure.exception.http.CMRSHTTPException;
+import org.multilinguals.enterprise.cmrs.interfaces.dto.SignInWithPasswordDTO;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,14 +31,15 @@ public class AuthorizationCommandController {
 
     /**
      * 使用密码登录
-     *
-     * @param command 密码登录命令
      */
     @PostMapping("/sign-in-with-password")
     @PreAuthorize("permitAll()")
-    public UserSignInDTO handle(@RequestBody SignInWithPasswordCommand command) {
+    public UserSignInDTO handle(@RequestBody @Validated SignInWithPasswordDTO dto) {
         try {
-            Tuple2<UserSessionId, UserId> result = commandGateway.sendAndWait(command);
+            Tuple2<UserSessionId, UserId> result = commandGateway.sendAndWait(new SignInWithPasswordCommand(
+                    dto.getIdInAccountType(),
+                    AccountType.ofValue(dto.getAccountType()),
+                    dto.getPassword()));
             return new UserSignInDTO(result.getT1().getIdentifier(), result.getT2().getIdentifier());
         } catch (AggregateNotFoundException ex) {
             throw new CMRSHTTPException(HttpServletResponse.SC_UNAUTHORIZED, ErrorCode.ACCOUNT_PASSWORD_INVALID);
---
+++ src/main/java/org/multilinguals/enterprise/cmrs/interfaces/dto/SignInWithPasswordDTO.java
@@ -0,0 +1,44 @@
+package org.multilinguals.enterprise.cmrs.interfaces.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class SignInWithPasswordDTO {
+    @NotNull(message = "")
+    private String idInAccountType;
+
+    @NotNull(message = "{account.type.NotNull}")
+    private Integer accountType;
+
+    @NotNull
+    private String password;
+
+    public SignInWithPasswordDTO(@NotNull String idInAccountType, @NotNull Integer accountType, @NotNull String password) {
+        this.idInAccountType = idInAccountType;
+        this.accountType = accountType;
+        this.password = password;
+    }
+
+    public String getIdInAccountType() {
+        return idInAccountType;
+    }
+
+    public void setIdInAccountType(String idInAccountType) {
+        this.idInAccountType = idInAccountType;
+    }
+
+    public Integer getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(Integer accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
