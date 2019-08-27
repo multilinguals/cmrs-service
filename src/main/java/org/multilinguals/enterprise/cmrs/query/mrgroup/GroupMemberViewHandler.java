664fdbe4d6b0a9ec320fc394d10061afe0efa7d2
t04041143 <t008008>
2019-08-27 13:43:41 +0800

    增加视图对删除事件的响应

--- src/main/java/org/multilinguals/enterprise/cmrs/query/mrgroup/GroupMemberViewHandler.java
+++ src/main/java/org/multilinguals/enterprise/cmrs/query/mrgroup/GroupMemberViewHandler.java
@@ -3,11 +3,14 @@ package org.multilinguals.enterprise.cmrs.query.mrgroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.eventhandling.Timestamp;
 import org.multilinguals.enterprise.cmrs.command.aggregate.mrgroup.event.MealReservationGroupCreatedEvent;
+import org.multilinguals.enterprise.cmrs.command.aggregate.mrgroup.event.MealReservationGroupDeletedEvent;
 import org.multilinguals.enterprise.cmrs.query.user.UserDetailsViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class GroupMemberViewHandler {
@@ -27,5 +30,12 @@ public class GroupMemberViewHandler {
             GroupMemberView groupMemberView = new GroupMemberView(owner.getId(), event.getId().getIdentifier(), owner.getRealName(), new Date(createdTime.toEpochMilli()));
             this.groupMemberViewRepository.save(groupMemberView);
         });
+    }
+
+    @EventHandler
+    public void on(MealReservationGroupDeletedEvent event) {
+        List<GroupMemberView> memberViews = this.groupMemberViewRepository.findAll(Example.of(new GroupMemberView(null, event.getId().getIdentifier(), null, null)));
+
+        this.groupMemberViewRepository.deleteAll(memberViews);
     }
 }
--- src/main/java/org/multilinguals/enterprise/cmrs/query/mrgroup/GroupOrderTakerViewHandler.java
+++ src/main/java/org/multilinguals/enterprise/cmrs/query/mrgroup/GroupOrderTakerViewHandler.java
@@ -3,11 +3,14 @@ package org.multilinguals.enterprise.cmrs.query.mrgroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.eventhandling.Timestamp;
 import org.multilinguals.enterprise.cmrs.command.aggregate.mrgroup.event.MealReservationGroupCreatedEvent;
+import org.multilinguals.enterprise.cmrs.command.aggregate.mrgroup.event.MealReservationGroupDeletedEvent;
 import org.multilinguals.enterprise.cmrs.query.user.UserDetailsViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class GroupOrderTakerViewHandler {
@@ -26,5 +29,12 @@ public class GroupOrderTakerViewHandler {
             GroupOrderTakerView groupOrderTakerView = new GroupOrderTakerView(owner.getId(), event.getId().getIdentifier(), owner.getRealName(), new Date(createdTime.toEpochMilli()));
             this.groupOrderTakerViewRepository.save(groupOrderTakerView);
         });
+    }
+
+    @EventHandler
+    public void on(MealReservationGroupDeletedEvent event) {
+        List<GroupOrderTakerView> groupOrderTakerViews = this.groupOrderTakerViewRepository.findAll(Example.of(new GroupOrderTakerView(null, event.getId().getIdentifier(), null, null)));
+
+        this.groupOrderTakerViewRepository.deleteAll(groupOrderTakerViews);
     }
 }
