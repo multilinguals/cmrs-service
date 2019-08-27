bab8e56ddbce3dcdf1fabd852906b21a626a8ccf
t04041143 <t008008>
2019-08-27 16:25:10 +0800

    增加查询点参团接口

--- src/main/java/org/multilinguals/enterprise/cmrs/interfaces/query/MealReservationGroupQueryController.java
+++ src/main/java/org/multilinguals/enterprise/cmrs/interfaces/query/MealReservationGroupQueryController.java
@@ -1,8 +1,25 @@
 package org.multilinguals.enterprise.cmrs.interfaces.query;
 
+import org.multilinguals.enterprise.cmrs.query.mrgroup.MealReservationGroupDetailsRepository;
+import org.multilinguals.enterprise.cmrs.query.mrgroup.MealReservationGroupDetailsView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MealReservationGroupQueryController {
+    private MealReservationGroupDetailsRepository mealReservationGroupDetailsRepository;
 
+    @Autowired
+    public MealReservationGroupQueryController(MealReservationGroupDetailsRepository mealReservationGroupDetailsRepository) {
+        this.mealReservationGroupDetailsRepository = mealReservationGroupDetailsRepository;
+    }
+
+    @GetMapping("/get-details-of-mr-group/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public MealReservationGroupDetailsView queryAllDishType(@PathVariable String id) {
+        return this.mealReservationGroupDetailsRepository.findById(id).orElseThrow(null);
+    }
 }
